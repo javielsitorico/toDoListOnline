8627fe29846139b4bfb797d23e40624b23d3dc04
javielsitorico <zx21student030@colegiovalledelmiro.es>
2022-12-14 12:09:33 +0100

    Empezado apartado de estilo de listas

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+comprobarTareasVacias();
+
 function comprobarTareasVacias() {
-     let parrafoElementoVacio = '';
+     let parrafoElementoVacio = document.createElement('p');
+     parrafoElementoVacio.classList.add('texto-secundario');
+     parrafoElementoVacio.innerHTML = 'There are currently no tasks';
      let listasTareas = document.getElementsByClassName('lista-tareas');
      for (let lista of listasTareas) {
           if(lista.childElementCount <= 0) {
-               lista.appendChild(parrafoElementoVacio)
+               lista.appendChild(parrafoElementoVacio.cloneNode(true));
           }
      }
 }
-
-comprobarTareasVacias();
