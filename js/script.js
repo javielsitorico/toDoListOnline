df128e2a7e688bf3db546e78a68a371e0cfef9ea
Javier <jlm03123@gmail.com>
2022-12-15 18:49:32 +0100

    Primera version completada ✔️

--- js/script.js
+++ js/script.js
@@ -12,29 +12,29 @@ function actualizarContenedoresVacios() {
       for (lista of listasTareas) {
             if (lista.childElementCount < 1) {
                   lista.innerHTML = (textoContenedorVacio);
-            } else if (lista.innerHTML != textoContenedorVacio && lista.childElementCount > 1){
+            } else if (lista.innerHTML != textoContenedorVacio && lista.childElementCount > 1) {
                   let contenidoNuevo = (lista.innerHTML).replace(textoContenedorVacio, '');
                   lista.innerHTML = contenidoNuevo;
             }
       }
 }
 
-function formatoTarea(nombre, duracion, estado) {
+function formatoTarea(nombre, duracion, etapa) {
       let accionBoton = null;
       let nombreAccionBoton = null;
 
-      switch (estado) {
+      switch (etapa) {
             case 0:
                   accionBoton = `empezarTarea('${nombre}')`;
-                  nombreAccionBoton = 'Begin Task';
+                  nombreAccionBoton = 'Begin';
                   break;
             case 1:
                   accionBoton = `marcarTareaAcabada('${nombre}')`;
-                  nombreAccionBoton = 'Complete Task ✔️';
+                  nombreAccionBoton = 'Done ✔️';
                   break;
             case 2:
                   return `
-                  <div class="tarea" id="${nombre}>
+                  <div class="tarea" id="${nombre}">
                         <p class="nombre-tarea">${nombre}</p>
                         <img src="media/svg/time.svg" alt="">
                         <p>: ${duracion} min</p>
@@ -60,16 +60,27 @@ function formatoTarea(nombre, duracion, estado) {
       `
 }
 
+function eliminarTarea(nombreTarea) {
+      document.getElementById(nombreTarea).remove();
+
+      if (tareas[nombreTarea][2]) {
+            clearTimeout(tareas[nombreTarea][2]);
+      }
+      delete tareas[nombreTarea];
+
+      actualizarContenedoresVacios();
+}
+
 function crearTarea() {
       let nombreNuevaTarea = document.getElementById('input--nombre-tarea');
       let tiempoTarea = document.getElementById('input--tiempo-tarea');
-      let estadoTarea = 0;
+      let etapaTarea = 0;
 
       if (nombreNuevaTarea.value in tareas || nombreNuevaTarea.value == '') {
-            nombreNuevaTarea.style.border = '1px solid #E02929'; 
+            nombreNuevaTarea.style.border = '1px solid #E02929';
             return;
       } else {
-            nombreNuevaTarea.style.border = 'none'; 
+            nombreNuevaTarea.style.border = 'none';
       }
 
       if (tiempoTarea.value < 1 || tiempoTarea.value > 360 || tiempoTarea.value == '') {
@@ -79,23 +90,60 @@ function crearTarea() {
             tiempoTarea.style.border = 'none';
       }
 
-      tareas[nombreNuevaTarea.value] = [tiempoTarea.value, estadoTarea];
-      let nuevaTarea = formatoTarea(nombreNuevaTarea.value, tiempoTarea.value, estadoTarea)
+      tareas[nombreNuevaTarea.value] = [parseInt(tiempoTarea.value), etapaTarea];
+      let nuevaTarea = formatoTarea(nombreNuevaTarea.value, parseInt(tiempoTarea.value), etapaTarea)
       listaParaHacer.innerHTML += nuevaTarea;
+
       actualizarContenedoresVacios();
 }
 
 function empezarTarea(nombreTarea) {
-      console.log(nombreTarea);
+      document.getElementById(nombreTarea).remove();
+
+      tareas[nombreTarea][1] = 1;
+      let nuevaTarea = formatoTarea(nombreTarea, tareas[nombreTarea][0], tareas[nombreTarea][1])
+      listaHaciendo.innerHTML += nuevaTarea;
+
+      tareas[nombreTarea][2] = setTimeout(
+            function () {
+                  notificionTimeOut(nombreTarea);
+            }, (parseInt(tareas[nombreTarea][0] * 60000))
+      );
+
+      actualizarContenedoresVacios();
+}
+
+function notificionTimeOut(nombreTarea) {
+      let nombre = document.getElementById(nombreTarea).querySelector('p.nombre-tarea');
+      nombre.style.color = '#A51A1A';
+      nombre.style.fontWeight = 'bold';
+      alert(`Time's over for ${nombreTarea}! Time to get another task done...`);
 }
 
 function marcarTareaAcabada(nombreTarea) {
+      document.getElementById(nombreTarea).remove();
+      if (tareas[nombreTarea][2]) {
+            clearTimeout(tareas[nombreTarea][2]);
+      }
 
+      tareas[nombreTarea][1] = 2;
+      let nuevaTarea = formatoTarea(nombreTarea, tareas[nombreTarea][0], tareas[nombreTarea][1])
+      listaHecho.innerHTML += nuevaTarea;
+
+      actualizarContenedoresVacios();
 }
 
-function eliminarTarea(nombreTarea) {
-      document.getElementById(nombreTarea).remove();
-      delete tareas[nombreTarea];
+function completarTareasHaciendo() {
+      for (let tarea in tareas) {
+            marcarTareaAcabada(tarea)
+      }
+      actualizarContenedoresVacios();
+}
+
+function eliminarTareasHechas() {
+      for (let tarea in tareas) {
+            eliminarTarea(tarea);
+      }
       actualizarContenedoresVacios();
 }
 
